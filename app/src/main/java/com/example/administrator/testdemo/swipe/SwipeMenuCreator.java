574e1ff2eea7071518894c3ad8aa0cb5b5bd3282
luoshuiquan <GITMNBvcxz3.14>
2016-06-13 13:05:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/testdemo/swipe/SwipeMenuCreator.java
@@ -0,0 +1,13 @@
+package com.example.administrator.testdemo.swipe;
+
+
+/**
+ * 
+ * @author baoyz
+ * @date 2014-8-24
+ *
+ */
+public interface SwipeMenuCreator {
+
+	void create(SwipeMenu menu);
+}
